8d8e32e9007fd4f49dbf93385f0ec6b93b7e909a
The-Real-Thisas <thisas@protonmail.com>
2021-05-30 08:58:46 +0400

    Code Reformat (Black)

--- test-convx.py
+++ test-convx.py
@@ -1,25 +1,33 @@
 from convx import *
 
+
 def test_convx_decimalToBinary():
-	assert decimalToBinary(25) == '11001'
+    assert decimalToBinary(25) == "11001"
+
 
 def test_convx_binaryToDecimal():
-	assert binaryToDecimal('11001') == 25
+    assert binaryToDecimal("11001") == 25
+
 
 def test_convx_decimalToHex():
-	assert decimalToHex(25) == '19'
+    assert decimalToHex(25) == "19"
+
 
 def test_convx_binaryToHex():
-	assert binaryToHex('11001') == '19'
+    assert binaryToHex("11001") == "19"
+
 
 def test_convx_addBinary():
-	assert addBinary('101', '101') == '1010'
+    assert addBinary("101", "101") == "1010"
+
 
 def test_convx_subBinary():
-	assert subBinary('1010', '101') == '101'
+    assert subBinary("1010", "101") == "101"
+
 
 def test_convx_twoDenaryToBinary():
-	assert twoDenaryToBinary(-25) == '100111'
+    assert twoDenaryToBinary(-25) == "100111"
+
 
 def test_convx_twoBinaryToDenary():
-	assert twoBinaryToDenary('100111') == -25
+    assert twoBinaryToDenary("100111") == -25
